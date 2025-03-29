beca965c46bb50f3dee364f2a8802d8d613f25db
Ehsan <dev.ehsanhosseini@gmail.com>
2025-03-29 18:09:16 +0330

    structure chaneges

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <script setup>
-import MenuBar from './pages/MenuBar.vue';
+import MenuBar from './components/MenuBar.vue';
 import { useStore } from './store/useStore';
 import { computed } from 'vue';
+
 const store = useStore();
 const isLoged = computed(() => store.isLoggedIn);
 </script>
-
 <template>
   <div>
     <MenuBar v-if="isLoged" />
     <router-view />
   </div>
 </template>
-
-<style scoped></style>
